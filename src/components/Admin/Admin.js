4162791ae4e0f3affd5d09e954cd1ea313f943c9
NDacHAI <142900142+NDacHAI@users.noreply.github.com>
2025-03-23 05:56:59 +0700

    admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,18 @@
-import Header from "../Header/Header";
+import Sidebar from "./Sidebar";
+import './Admin.scss'
+import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
 
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <div>
-            <h1>Admin Page</h1>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <Sidebar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+            </div>
         </div>
     );
 }
