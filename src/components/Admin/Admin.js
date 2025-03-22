8854ff039fffe367ba3832c05e1870b42375f358
NDacHAI <142900142+NDacHAI@users.noreply.github.com>
2025-03-23 02:24:22 +0700

    route and home page

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const Admin = (props) => {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+        </div>
+    );
+}
+
+export default Admin;
---
+++ src/components/Users/User.js
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+
+const User = (props) => {
+    return (
+        <div>
+            <div>
+                User
+            </div>
+        </div>
+    );
+}
+
+export default User;
