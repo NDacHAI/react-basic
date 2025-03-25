89a29522db0720577effa43bf73eee8d3e39df19
NDacHAI <142900142+NDacHAI@users.noreply.github.com>
2025-03-26 04:23:29 +0700

    admin create user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,19 +1,33 @@
 import ModelCreateUser from './ModelCreateUser'
+import './ManageUser.scss'
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useState } from 'react';
 
-const ManageUser = () => {
+
+const ManageUser = (props) => {
+
+    const [showModelCreateUser, setShowModelCreateUser] = useState(false);
+
     return (
-        <div>
-            <div className="manage-user-container">
-                <div className="title">Manage User</div>
-                <div className="users-content">
-                    <div>
-                        <button>Add new user</button>
-                    </div>
-                    <div>
-                        Table user
-                        <ModelCreateUser />
-                    </div>
+        <div className="manage-user-container">
+            <div className="title">Manage User</div>
+            <div className="users-content">
+                <div className='btn-add-new'>
+                    <button
+                        className='btn btn-primary'
+                        onClick={() => setShowModelCreateUser(true)}
+                    >
+                        <AiFillPlusCircle />
+                        Add new user
+                    </button>
                 </div>
+                <div className='table-users-container'>
+                    Table user
+                </div>
+                <ModelCreateUser
+                    show={showModelCreateUser}
+                    setShow={setShowModelCreateUser}
+                />
             </div>
         </div>
     );
--- src/components/Admin/Content/ModelCreateUser.js
+++ src/components/Admin/Content/ModelCreateUser.js
@@ -1,57 +1,163 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { AiFillPlusCircle } from "react-icons/ai";
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 
 
-const ModelCreateUser = () => {
-    const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
+const ModelCreateUser = ({ show, setShow }) => {
+
+    const handleClose = () => {
+        setShow(false)
+        setEmail('')
+        setPassword('')
+        setUserName('')
+        setImage('')
+        setRole('USER')
+        setPreviewImage('')
+    };
     const handleShow = () => setShow(true);
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [userName, setUserName] = useState('')
+    const [role, setRole] = useState('USER')
+    const [image, setImage] = useState('')
+    const [previewImage, setPreviewImage] = useState("")
+
+    const handleSubmit = async () => {
+
+        const isValidEmail = validateEmail(email)
+        if (!isValidEmail) {
+            toast.error('Invalid Email');
+            return;
+        }
+
+        if (!password) {
+            toast.error('Invalid Password');
+            return;
+        }
+        const data = new FormData();
+        data.append('email', email);
+        data.append('password', password);
+        data.append('userName', userName);
+        data.append('role', role);
+        data.append('userImage', image);
+        const res = await axios.post('http://localhost:8081/api/v1/participant', data)
+
+        if (res.data && res.data.EC === 0) {
+            toast.success(res.data.EM);
+            handleClose()
+        }
+
+        if (res.data && res.data.EC !== 0) {
+            toast.error(res.data.EM);
+        }
+
+    }
+
+    const handleUpLoadImage = (e) => {
+        if (e.target && e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0])
+            setPreviewImage(URL.createObjectURL(e.target.files[0]))
+        }
+    }
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
 
-            <Modal show={show} onHide={handleClose} size='xl' backdrop='static'>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                size='xl' backdrop='static'
+                className='model-add-user'
+            >
+
                 <Modal.Header closeButton>
                     <Modal.Title>Add new user</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <form className="row g-3">
                         <div className="col-md-6">
-                            <label for="inputEmail4" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="inputEmail4" />
+                            <label htmlFor="inputEmail4" className="form-label">Email</label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="inputEmail4"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div className="col-md-6">
-                            <label for="inputPassword4" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="inputPassword4" />
+                            <label htmlFor="inputPassword4" className="form-label">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="inputPassword4"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
 
                         <div className="col-md-6">
-                            <label for="inputUsername" className="form-label">Username</label>
-                            <input type="text" className="form-control" id="inputUsername" />
+                            <label htmlFor="inputUsername" className="form-label">Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="inputUsername"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                            />
                         </div>
                         <div className="col-md-4">
-                            <label for="inputState" className="form-label">Role</label>
-                            <select id="inputState" className="form-select">
-                                <option selected value="USER">USERS</option>
+                            <label htmlFor="inputState" className="form-label">Role</label>
+                            <select
+                                id="inputState"
+                                className="form-select"
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                            >
+                                <option defaultValue value="USER">USERS</option>
                                 <option value="ADMIN">ADMIN</option>
                             </select>
                         </div>
                         <div className="col-md-12">
-                            <label for="inputImage" className="form-label">Image</label>
-                            <input type="file" className="form-control" id="inputImage" />
+                            <label htmlFor="inputImage" className="form-label label-upload">
+                                <AiFillPlusCircle />
+                                Upload file image
+                            </label>
+                            <input
+                                type="file"
+                                hidden
+                                id='inputImage'
+                                onChange={(e) => handleUpLoadImage(e)}
+                            />
+                        </div>
+
+                        <div className="col-md-12 img-preview">
+                            {previewImage ?
+                                (<img src={previewImage} alt='preview' className="img-preview" />)
+                                :
+                                (<div id='inputImage'>Preview image</div>)
+                            }
+
                         </div>
                     </form></Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleSubmit}>
                         Add
                     </Button>
                 </Modal.Footer>
